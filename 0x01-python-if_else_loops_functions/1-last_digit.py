cdca3020da2fd4b4dfbba8ef8e8b70276107ca79
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-04-28 13:03:22 -0500

    fix neg number show

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 n = int(repr(number)[-1])
-
+if number < 0:
+    n = n * -1
 if n > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, n))
-
 elif n == 0:
     print("Last digit of {} is {} and is 0".format(number, n))
-
 elif n < 6:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, n))
 
