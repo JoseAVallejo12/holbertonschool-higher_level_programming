2504742863e60d2ed4fc5bcc34288968d99d2e8c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-04-28 14:08:03 -0500

    dd

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 n = abs(number) % 10
-less = "and is less than 6 and not 0"
-greater = "and is grater than 5"
-
 if number < 0:
     n = n * -1
 print("Last digit of", end=" ")
