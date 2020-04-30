642d3d68516a85956aeead11ebdc7d1a61b63fae
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-04-30 18:53:12 -0500

    fix task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,21 +5,22 @@ if __name__ == "__main__":
 
     argc = len(argv)
     op = {'+', '-', '*', '/'}
-    a = int(argv[1])
-    b = int(argv[3])
 
     if argc != 4:
         print("sage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        if not argv[2] in op:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        a = int(argv[1])
+        b = int(argv[3])
         if argv[2] == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
-        if argv[2] == '-':
+        elif argv[2] == '-':
             print("{} + {} = {}".format(a, b, sub(a, b)))
-        if argv[2] == '*':
+        elif argv[2] == '*':
             print("{} + {} = {}".format(a, b, mul(a, b)))
-        if argv[2] == '/':
+        elif argv[2] == '/':
             print("{} + {} = {}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
