ec3e6dd7ee90a849913f88dedc50dbcec189d075
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-04-30 19:08:22 -0500

    fix task more

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
         if argv[2] == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
         elif argv[2] == '-':
-            print("{} + {} = {}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif argv[2] == '*':
-            print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif argv[2] == '/':
-            print("{} + {} = {}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
