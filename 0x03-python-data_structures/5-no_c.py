e489d058f5566b3f8f1fc4f85305fcf1cc19afee
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-05 14:38:37 -0500

    fix problem copy string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        return (my_string.translate({ord(i): None for i in 'Cc'}))
-    return None
+        new_str = my_string
+        return (new_str.translate({ord(i): None for i in 'Cc'}))
+    new_str = my_string
+    return new_str
