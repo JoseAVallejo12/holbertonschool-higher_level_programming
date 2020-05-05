3694ea315f784f0bfed0193da69f55587940f7ef
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-05 12:35:19 -0500

    fix problem return task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return
-    for i in range(len(my_list)):
-        if i == idx:
-            print(my_list[idx])
-            return my_list[idx]
-    return
+    if idx < 0 or idx > len(my_list):
+        return None
+    print(my_list[idx])
