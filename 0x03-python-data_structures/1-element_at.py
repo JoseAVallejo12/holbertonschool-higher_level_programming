5cf9ce837c3036aadcb96bdc152d72d704b05ac5
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-05 12:27:58 -0500

    create task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    print(my_list[idx])
+    if idx < 0:
+        return
+    for i in range(len(my_list)):
+        if i == idx:
+            print(my_list[idx])
+            return my_list[idx]
+    return
