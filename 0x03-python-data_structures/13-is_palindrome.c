535395f75ae8ea452e8ee6d5f6bdb70cbc81c664
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-05 21:05:19 -0500

    fix betty style task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,37 +6,37 @@
  */
 int is_palindrome(listint_t **head)
 {
-    int i = 0, j;
-    listint_t *aux = *head;
-    int *datos = NULL;
-    /* empty list is palindorme */
-    if (head == NULL)
-        return (1);
-    /* length the single list linked */ 
-    while (aux != NULL)
-    {
-        aux = aux->next; 
-        i++;
-    }
-    /* reset var and ponter for get data of linked list */
-    aux = *head;
-    datos = (int *)malloc(sizeof(int) + i);
-    if (datos == NULL)
-        return (0);
-    
-    i = 0;
-    while (aux != NULL)
-    {
-        datos[i] = aux->n;
-        aux = aux->next;
-        i++;
-    }    
-    for (j = 0; i > 0; j++)
-    {
-        i--;
-        if (datos[j] != datos[i])
-            return (0);
-        
-    }
-    return (1);
+	int i = 0, j;
+	listint_t *aux = *head;
+	int *datos = NULL;
+	/* empty list is palindorme */
+	if (head == NULL)
+		return (1);
+	/* length the single list linked */
+	while (aux != NULL)
+	{
+		aux = aux->next;
+		i++;
+	}
+	/* reset var and ponter for get data of linked list */
+	aux = *head;
+	datos = (int *)malloc(sizeof(int) + i);
+	if (datos == NULL)
+		return (0);
+
+	i = 0;
+	while (aux != NULL)
+	{
+		datos[i] = aux->n;
+		aux = aux->next;
+		i++;
+	}
+	for (j = 0; i > 0; j++)
+	{
+		i--;
+		if (datos[j] != datos[i])
+			return (0);
+
+	}
+	return (1);
 }
