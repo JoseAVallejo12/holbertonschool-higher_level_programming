df9dfc2426c2d0d6d27000bf17c6fb1464879bbc
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-07 15:51:49 -0500

    try without copy

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in sorted(a_dictionary.keys()):
-        new_dic = a_dictionary.copy()
-        del new_dic[key]
-        return new_dic
+        del a_dictionary[key]
     return a_dictionary
