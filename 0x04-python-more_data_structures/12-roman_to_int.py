2f9a5878c73a4754b205b7e58ec62faa3c11e342
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-07 18:48:40 -0500

    order code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
-        resul = 0
-        ciclos = 0
 
-        roman = {
-            'I': 1, 'V': 5,
-            'X': 10, 'L': 50,
-            'C': 100, 'D': 500,
-            'M': 1000
-            }
-        roman_chars = sorted(roman.keys())
+    if type(roman_string) is not str or len(roman_string) is 0:
+        return 0
 
-        if type(roman_string) is not str or len(roman_string) is 0:
-                return 0
+    roman = {
+        'I': 1, 'V': 5,
+        'X': 10, 'L': 50,
+        'C': 100, 'D': 500,
+        'M': 1000
+    }
+    resul = 0
+    ciclos = 0
 
-        for roman_key in list(roman_string):
-            if roman_key not in roman_chars:
-                return 0
-            if ciclos > 0 and resul < roman[roman_key]:
-                resul = roman[roman_key] - resul
-            else:
-                resul = resul + roman[roman_key]
-            ciclos += 1
-        return resul
-
-    return 0
+    for roman_key in list(roman_string):
+        if ciclos > 0 and resul < roman[roman_key]:
+            resul = roman[roman_key] - resul
+        else:
+            resul = resul + roman[roman_key]
+        ciclos += 1
+    return resul
