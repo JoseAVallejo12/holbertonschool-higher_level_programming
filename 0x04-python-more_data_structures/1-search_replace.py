8278420fae8be6d91cd368357dad3cc027e5c8c7
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-07 12:55:49 -0500

    delete check if my_list is diferent of null

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/phython3
 def search_replace(my_list, search, replace):
-    if my_list:
-        new = []
-        new = list(map((lambda x: replace if x == search else x), my_list))
-        return new
+    new = []
+    new = list(map((lambda x: replace if x == search else x), my_list))
+    return new
