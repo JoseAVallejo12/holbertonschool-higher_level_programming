0a013bfa586fca58a253fdc929f393e3d317a006
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-07 12:58:40 -0500

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/phython3
 def search_replace(my_list, search, replace):
     new = []
-    new = list(map((lambda x: replace if x == search else x), my_list))
+    new = list(map(lambda x: replace if x == search else x, my_list))
     return new
