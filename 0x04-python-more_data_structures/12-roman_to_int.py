3b0d3bbdcaab77c93d9df65abfe3c085013a446a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-07 18:26:53 -0500

    validate if in  is string o null

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,16 +7,8 @@ def roman_to_int(roman_string):
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         roman_chars = sorted(roman.keys())
 
-        for i in roman_chars:
-            count = 0
-            for j in list(roman_string):
-                if i == j:
-                    count += 1
-                else:
-                    count = 0
-                    
-                if count > 3:
-                    return 0
+        if type(roman_string) is not str or len(roman_string) is 0:
+                return 0
 
         for roman_values in list(roman_string):
             if not roman_values in roman_chars:
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -12,8 +12,8 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "IX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = ""
+roman_number = "LXXXVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "MMMDCCCLLLMXXXLVVVIII"
+roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
