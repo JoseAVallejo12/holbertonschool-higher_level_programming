ad948259eb40d5f438f562ab1a1c036159bf9cfe
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-18 12:48:12 -0500

    fix problem imprint error

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -2,7 +2,7 @@
 safe_print_list_integers = \
     __import__('2-safe_print_list_integers').safe_print_list_integers
 
-my_list = [1, 2, 3, 4, 5]
+my_list = ['o', 2, 3, 4, 5]
 
 nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     num = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            num += 1
-        except:
-            if (TypeError):
-                continue
-            if (IndexError):
-                print()
-                return num
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                num += 1
+        except TypeError:
+            break
     print()
     return num
