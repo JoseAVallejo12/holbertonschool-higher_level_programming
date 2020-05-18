136dd888770dae8ee342d9b01968dfa360bc8d52
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-18 14:43:09 -0500

    fix error print format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = int(a / b)
+        result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: ".format(a, b, result))
+        print("Inside result: {}".format(result))
     return result
