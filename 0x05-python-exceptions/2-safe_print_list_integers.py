e396fdbba4cdff088e9104c0fa169d59552b943a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-18 12:21:55 -0500

    fix value error check

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             num += 1
         except:
-            if (IndexError):
+            if (ValueError):
                 continue
             else:
                 return num
