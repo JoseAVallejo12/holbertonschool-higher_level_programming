217f121e448a1edfa11f70b78950fd7f55b180d3
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-19 17:48:37 -0500

    other validate form

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,9 +33,12 @@ class Square(object):
         return self.__position
 
     @position.setter
-    def position(self, x):
+    def position(self, value):
         """ setter method for private position """
-        if type(x) is not tuple or type(x[0] and x[1]) is not int:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
