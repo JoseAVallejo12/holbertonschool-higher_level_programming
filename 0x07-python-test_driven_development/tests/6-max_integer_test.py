413679e0b366f3825345c7eefd95e36112451655
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-22 21:48:22 -0500

    update proyect

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,14 @@
-"""unit test for max_integer function"""
+""" unit test for max_integer function """
 
 import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaximumInteger(unittest.TestCase):
-    """unit testing class"""
+    """
+        unit testing class 
+    """
 
     def test_with_no_input(self):
         self.assertEqual(max_integer(), None)
@@ -16,14 +19,17 @@ class TestMaximumInteger(unittest.TestCase):
 
     def test_float(self):
         lst = [1.2, 2.3, 3.4, 50.8]
-        self.assertEqual(max_integer(lst), 50.2)
+        self.assertEqual(max_integer(lst), 50.8)
 
     def test_mixed_integers(self):
         lst = [-1, -2, 3, -50]
         self.assertEqual(max_integer(lst), 3)
 
-    def test_non_valid_list(self):
-        lst = ['hello', 0, 0, 0]
-        with self.assertRaises(TypeError):
-            max_integer(lst)
+    def test_tuple_data(self):
+        lst = (-1, -2, 3, -50)
+        self.assertEqual(max_integer(lst), 3)
 
+    def test_non_valid_list(self):
+        new_list = ['hello', 0, 0, 0]
+        with self.assertRaises(TypeError):
+            max_integer(new_list)
