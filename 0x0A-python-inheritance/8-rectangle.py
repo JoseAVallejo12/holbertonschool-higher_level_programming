68e401fbaf75d11341fc69bdeee570d8a9272dff
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-02 20:17:13 -0500

    fix name var heigth change for height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
+""" create a new class """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """ class rectangle inherits of besegeometry """
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
         """
-        self.integer_validator("width", width)
+            Intialize a new Rectangle.
+            Args:
+                width (int): The width of the new Rectangle.
+                height (int): The height of the new Rectangle.
+        """
+
+        self.integer_validator('width', width)
         self.__width = width
-        self.integer_validator("height", height)
+        self.integer_validator('height', height)
         self.__height = height
