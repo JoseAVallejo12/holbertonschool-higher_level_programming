45246f4cca2e8889843521fd0fa3dedee547ab79
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-03 18:37:18 -0500

    more changes

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,7 +3,6 @@ import sys
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-
 filename = "add_item.json"
 
 try:
@@ -13,5 +12,4 @@ except:
 finally:
     for idx in range(1, len(sys.argv)):
         my_list.append(sys.argv[idx])
-
     save_to_json_file(my_list, filename)
