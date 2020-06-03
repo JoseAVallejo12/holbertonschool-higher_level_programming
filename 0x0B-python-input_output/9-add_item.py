82d1a77f27b2d160cb78fe744128ae98bfeb6957
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-03 18:40:54 -0500

    add comment to script

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+funtion for read and safe argv in
+Json format in file
+"""
 import sys
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
