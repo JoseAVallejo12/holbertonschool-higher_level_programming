241d5aa52ca01745054c50db1e49011d57828ea2
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-03 19:28:10 -0500

    create task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 
+Returns simple data structure type dict
+(list, dictionary, string, integer and boolean) 
+for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """new funtion
+
+    Args:
+        obj ([object]): is instance of an class
+
+    Returns:
+        [dict]: Json format
+    """
+    return (obj.__dict__)
