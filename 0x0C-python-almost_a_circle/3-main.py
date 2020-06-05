e718ffd82d28edb13ce8fdd8988965c0dd7bdfbd
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-05 13:55:16 -0500

    create folders for unitest case file base and rectangle.py

--- 0x0C-python-almost_a_circle/3-main.py
+++ 0x0C-python-almost_a_circle/3-main.py
@@ -7,8 +7,13 @@ if __name__ == "__main__":
     r1 = Rectangle(3, 2)
     print(r1.area())
 
+    r3 = Rectangle(8, 7, 0, 0, 2)
+    print(r3.area())
+
     r2 = Rectangle(2, 10)
     print(r2.area())
 
-    r3 = Rectangle(8, 7, 0, 0, 12)
-    print(r3.area())
+
+    
+
+    print(r1.id, r3.id, r2.id)
