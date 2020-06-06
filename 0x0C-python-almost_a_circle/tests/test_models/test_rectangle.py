0de7732201849ad61eaed0ac1b5c0ce86f99948d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-06 15:55:01 -0500

    test rectanfle area task 4

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,40 +10,40 @@ from models.base import Base
 class TestTask_Two(unittest.TestCase):
     """ unit testing class Rectangle task 2. First Rectangle """
 
-    def test_a01_id(self):
+    def test_aa1_id(self):
         """ check count and assignation of id in empty argvs """
         self.assertEqual(Rectangle(5, 5, 0, 0, 1).id, 1)
-        self.assertTrue(Rectangle(5, 5, 0, 0).id == 1)
-        self.assertTrue(Rectangle(5, 5, 0, 0).id == 2)
+        self.assertEqual(Rectangle(5, 5, 0, 0).id, 3)
+        self.assertEqual(Rectangle(5, 5, 0, 0).id, 4)
         self.assertEqual(Rectangle(5, 5, 0, 0, "holberton").id, "holberton")
 
-    def test_a02_instance(self):
+    def test_aa2_instance(self):
         """ check inherence and subclass """
         x = Rectangle(5, 5, 0, 0)
         self.assertTrue(
             type(x) is Rectangle and issubclass(type(x), Base))
 
-    def test_a03_private_width(self):
+    def test_aa3_private_width(self):
         """ validate private attribute for __witdth """
         with self.assertRaises(AttributeError):
             print(Rectangle(5, 5, 0, 0, 1).__width)
 
-    def test_a04_private_height(self):
+    def test_aa4_private_height(self):
         """ validate private attribute for __height """
         with self.assertRaises(AttributeError):
             print(Rectangle(5, 5, 0, 0, 1).__height)
 
-    def test_a05_private_x(self):
+    def test_aa5_private_x(self):
         """ validate private attribute for _x """
         with self.assertRaises(AttributeError):
             print(Rectangle(5, 5, 0, 0, 1).__x)
 
-    def test_a06_private_y(self):
+    def test_aa6_private_y(self):
         """ validate private attribute for _y """
         with self.assertRaises(AttributeError):
             print(Rectangle(5, 5, 0, 0, 1).__y)
 
-    def test_a07_getter(self):
+    def test_aa7_getter(self):
         """ test getters and setters method """
         gt = Rectangle(5, 7, 3, 8, 20)
         self.assertEqual(gt.width, 5)
@@ -52,7 +52,7 @@ class TestTask_Two(unittest.TestCase):
         self.assertEqual(gt.y, 8)
         self.assertEqual(gt.id, 20)
 
-    def test_a08_setters(self):
+    def test_aa8_setters(self):
         gt = Rectangle(4, 5, 8, 1, 20)
         gt.width = 30
         self.assertEqual(gt.width, 30)
@@ -67,7 +67,7 @@ class TestTask_Two(unittest.TestCase):
 class TestTask_Three(unittest.TestCase):
     """ unit testing class Rectangle task 3. Validate attributes """
 
-    def test_a09_TypeError_width(self):
+    def test_ab1_TypeError_width(self):
         """ check TypeError for witdh """
         with self.assertRaises(TypeError):
             Rectangle('q', 6, 0, 0, 0)
@@ -84,7 +84,10 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(None, 5, 0, 0, 0)
 
-    def test_a10_TypeError_height(self):
+        with self.assertRaises(TypeError):
+            Rectangle(True, 3, 0, 0, 0)
+
+    def test_ab2_TypeError_height(self):
         """ check ValueError for height """
         with self.assertRaises(TypeError):
             Rectangle(6, 'j', 0, 0, 0)
@@ -101,7 +104,10 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(5, None, 0, 0, 0)
 
-    def test_a11_TypeError_x(self):
+        with self.assertRaises(TypeError):
+            Rectangle(3, True, 0, 0, 0)
+
+    def test_ab3_TypeError_x(self):
         """ check ValueError for x """
         with self.assertRaises(TypeError):
             Rectangle(6, 6, 'x', 0, 0)
@@ -118,7 +124,10 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(5, 5, None, 0, 0)
 
-    def test_a11_TypeError_y(self):
+        with self.assertRaises(TypeError):
+            Rectangle(3, 4, True, 0, 0)
+
+    def test_ab4_TypeError_y(self):
         """ check ValueError for x """
         with self.assertRaises(TypeError):
             Rectangle(6, 6, 2, 'y', 0)
@@ -135,7 +144,10 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(5, 5, 4, None, 0)
 
-    def test_a12_ValueError_width(self):
+        with self.assertRaises(TypeError):
+            Rectangle(3, 4, 5, True, 0)
+
+    def test_ab5_ValueError_width(self):
         """ check ValueError for width """
         with self.assertRaises(ValueError):
             Rectangle(-5, 5, 0, 0, 1)
@@ -143,7 +155,7 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(ValueError):
             Rectangle(0, 5, 0, 0, 1)
 
-    def test_a13_ValueError_height(self):
+    def test_ab6_ValueError_height(self):
         """ check ValueError for height """
         with self.assertRaises(ValueError):
             Rectangle(5, -5, 0, 0, 1)
@@ -151,13 +163,26 @@ class TestTask_Three(unittest.TestCase):
         with self.assertRaises(ValueError):
             Rectangle(5, 0, 0, 0, 1)
 
-    def test_a13_ValueError_x(self):
+    def test_ab7_ValueError_x(self):
         """ check ValueError for x """
         with self.assertRaises(ValueError):
             Rectangle(5, 5, -1, 0, 1)
 
-    def test_a13_ValueError_y(self):
+    def test_ab8_ValueError_y(self):
         """ check ValueError for x """
         with self.assertRaises(ValueError):
             Rectangle(5, 5, 4, -2, 1)
 
+
+class TestTask_Four(unittest.TestCase):
+    """ unit testing class Rectangle task 4. Area first """
+
+    def test_ac1_area1(self):
+        """ check value resul 4x5 = 20 """
+        c = Rectangle(4, 5)
+        self.assertEqual(c.area(), 20)
+
+    def test_ac2_area2(self):
+        """ check value resul 4x5 = 20 """
+        c = Rectangle(8, 5)
+        self.assertEqual(c.area(), 40)
