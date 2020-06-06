b1ab4c94498cd71314104c00214b1cb8e5c04057
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-05 21:17:01 -0500

    update unitest task 3

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 """ unit test for max_integer function """
 
 import unittest
-
-#Base = __import__('base.py').Base
+# Base = __import__('base.py').Base
 from models.base import Base
 
 
@@ -14,21 +14,21 @@ class TestClassBase(unittest.TestCase):
     def test_a001(self):
         """ check empty argument send to function """
         b1 = Base()
-        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b2.id, 2)
         b3 = Base()
-        self.assertEqual(b3.id, 3)
         b4 = Base(None)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
         self.assertEqual(b4.id, 4)
 
     def test_a002(self):
         """ check with argument send to function"""
         b5 = Base(13)
-        self.assertEqual(b5.id, 13)
         b6 = Base('s')
-        self.assertEqual(b6.id, 's')
         b7 = Base("holberton")
+        self.assertEqual(b5.id, 13)
+        self.assertEqual(b6.id, 's')
         self.assertEqual(b7.id, "holberton")
 
     def test_a003(self):
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" unit test for max_integer function """
+import unittest
+
+# Rectangle = __import__('base.py').Rectangle
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestClassRectangle(unittest.TestCase):
+    """
+        unit testing class Rectangle
+    """
+
+    def test_a001(self):
+        """ Checks for task 2. First Rectangle """
+
+        a = Rectangle(5, 5, 0, 0, 1)
+
+        """ check id object """
+        self.assertEqual(a.id, 1)
+
+        """ check inherence and subclass """
+        self.assertTrue(type(a) is Rectangle and issubclass(type(a), Rectangle))
+
+        """ check count and assignation of id in empty argvs """
+        b1 = Rectangle(5, 5, 0, 0)
+        b2 = Rectangle(5, 5, 0, 0)
+        b3 = Rectangle(5, 5, 0, 0, "holberton")
+        self.assertTrue(b1.id == 1)
+        self.assertTrue(b2.id == 2)
+        self.assertEqual(b3.id, "holberton")
+
+class TestOtro(unittest.TestCase):
+
+    def test_a002(self):
+        """ Checks for task 3. Validate attributes """
+
+        """ check TypeError for witdh, height, x and y """
+        with self.assertRaises(TypeError):
+            Rectangle('q', (1, 3), 0, 0, 0)
+
+        with self.assertRaises(TypeError):
+            Rectangle([1, 5], 'u', 0, 0, 0)
+
+        with self.assertRaises(TypeError):
+            Rectangle("hola", {'v': 1, 'c': 3}, 0, 0, 0)
+
+        with self.assertRaises(TypeError):
+            Rectangle("hola", None, 0, 0, 0)
+
+        with self.assertRaises(TypeError):
+            Rectangle(None)
+
+        """ check ValueError for witdh, height, x and y """
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2, 0, 0)
+            
+'''
+
+    def test_a003(self):
+        pass
+
+
+    def test_a004(self):
+        pass
+'''
