b871bcd254359c5105e9c1c3b7aa750f8bdf6cba
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-09 17:32:09 -0500

    update task 19 and create task 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,13 +62,13 @@ class Base(object):
 
     @staticmethod
     def from_json_string(json_string):
-        """convert json to python object
+        """ convert json to python object
 
-        Args:
-            json_string (str): string to convert
+            Args:
+                json_string (str): string to convert
 
-        Returns:
-            list: ret
+            Returns:
+                list: ret
         """
 
         if json_string is None:
@@ -78,10 +78,10 @@ class Base(object):
 
     @classmethod
     def create(cls, **dictionary):
-        """create objeto dummi
+        """ create objeto dummi
 
-        Returns:
-            objet: object is intance of call funtion create
+            Returns:
+                objet: object is intance of call funtion create
         """
 
         if dictionary and dictionary is not {}:
@@ -94,19 +94,63 @@ class Base(object):
 
     @classmethod
     def load_from_file(cls):
-        """load file json format
-
-        Returns:
-            list: list of object create
         """
+            load_from_file method:
+            a method that returns a list of instances.
+        """
+        filename = cls.__name__ + ".json"
+        newlist = []
+        if cls is None:
+            return newlist
+        try:
+            with open(filename, mode="r", encoding='utf-8') as Myfile:
+                newlist = cls.from_json_string(Myfile.read())
+            for item in range(len(newlist)):
+                newlist[item] = cls.create(**newlist[item])
+        except Exception:
+            pass
+        return newlist
 
-        file = cls.__name__ + '.json'
-        with open(file, mode='r', encoding='utf-8') as myfile:
-            if myfile is None:
-                return("[]")
-            else:
-                new_list = cls.from_json_string(myfile.read())
-                new_obj = []
-                for obj in new_list:
-                    new_obj.append(cls.create(**obj))
-                return new_obj
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        writes a object's list string representation
+        into a CVS file
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode='w', encoding='utf-8') as file:
+            string = csv.writer(file)
+            if cls.__name__ is "Square":
+                for i in list_objs:
+                    string.writerow([i.id, i.size, i.x, i.y])
+            elif cls.__name__ is "Rectangle":
+                for i in list_objs:
+                    string.writerow([i.id, i.width, i.height, i.x, i.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        reads from a CVS file an object's list
+        string representation.
+        """
+        filename = cls.__name__ + ".csv"
+        mylist = []
+        try:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                read = csv.reader(file)
+                for i in read:
+                    if cls.__name__ is "Square":
+                        dict1 = {
+                            "id": int(i[0]), "size": int(i[1]),
+                            "x": int(i[2]), "y": int(i[3])
+                        }
+                    elif cls.__name__ is "Rectangle":
+                        dict1 = {
+                            "id": int(i[0]), "width": int(i[1]),
+                            "height": int(i[2]), "x": int(i[3]),
+                            "y": int(i[4])
+                        }
+                    mylist.append(cls.create(**dict1))
+            return mylist
+        except:
+            return []
