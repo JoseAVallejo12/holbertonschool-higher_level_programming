a5796541aa03adf888a5b40caf34ed295963f0a2
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 13:33:50 -0500

    more fix problem

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Script that creates the database hbtn_0d_2 and the user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
