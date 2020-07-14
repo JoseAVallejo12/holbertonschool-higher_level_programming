188608dc2af1621ad009db377562b0feac9c74c3
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 15:55:11 -0500

    MORE FIXESS

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- Creates the database hbtn_0d_usa and the table states on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
   id INT NOT NULL UNIQUE AUTO_INCREMENT,
   PRIMARY KEY (id),
   state_id INT NOT NULL,
-  FOREIGN KEY (state_id) REFERENCES states (id),
+  FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id),
   name VARCHAR (256) NOT NULL,
 );
