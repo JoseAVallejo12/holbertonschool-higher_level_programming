26b9eb21e2c2a414bc63788e6b500788154fca29
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 15:51:40 -0500

    update code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,11 +1,9 @@
 -- Creates the database hbtn_0d_usa and the table states on your MySQL server.
-CREATE DATABASE
-IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE
-IF NOT EXISTS hbtn_0d_usa.cities
-(
-	id INT NOT NULL UNIQUE AUTO_INCREMENT,
-	state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES state(id),
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+  id INT NOT NULL UNIQUE AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  state_id INT NOT NULL,
+  FOREIGN KEY (state_id) REFERENCES states (id),
+  name VARCHAR (256) NOT NULL,
 );
