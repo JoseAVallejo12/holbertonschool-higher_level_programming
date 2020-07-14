4fa24bf2f0f588e921557e2c6322e1096f0d5721
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 16:02:35 -0500

    delete attr uniq

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Create a database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT NOT NULL UNIQUE AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
   state_id INT NOT NULL,
   FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id),
