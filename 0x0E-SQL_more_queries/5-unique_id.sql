389cda428089ee062492cfc6768ca99ab5d41ed0
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 14:52:06 -0500

    fi name table to create

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
--- Creates the table force_name on your MySQL server.
+-- Creates the table unique_id on your MySQL server.
 CREATE TABLE
-IF NOT EXISTS id_not_null
+IF NOT EXISTS unique_id
 (
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256) NOT NULL
