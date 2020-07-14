d9bf33fe4bcd09dca217999f32df49cfcbdecc75
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 15:58:55 -0500

    fix coment line one

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- Creates the database hbtn_0d_usa and the table states on your MySQL server.
+-- Create a database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
   id INT NOT NULL UNIQUE AUTO_INCREMENT,
