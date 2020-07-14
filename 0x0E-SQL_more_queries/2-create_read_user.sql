da83305f1da31e32b40612cb21b97b6cde9ac280
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-14 13:57:10 -0500

    fix privilegies

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+-- Creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON 'hbtn_0d_2' TO 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
