185ac99d87445cbb28dab06c5e6c40ed09e00248
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-13 20:06:08 -0500

    more fix syntasys

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
--- Creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR (256), score INT);
+-- Creates an second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "Jhon", 10),
 	(2, "Alex", 3),
