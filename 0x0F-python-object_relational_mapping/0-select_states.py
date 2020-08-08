1b7fc9ff4c90c2515be0356fc657fa8c6b2cdd5a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-08 09:18:17 -0500

    add commet for funtion

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,20 +3,28 @@
 import MySQLdb
 from sys import argv
 
+# connect to db
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset="utf8"
-        )
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
 
+    # Getting a cursor in MySQLdb python
     cur = conn.cursor()
+
+    # Executing db queries
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Obtaining all result of queries
     query_table = cur.fetchall()
+    # Printing the result one to one
     for row in query_table:
         print(row)
+
+    # Close conection to cursor and db
     cur.close()
     conn.close()
