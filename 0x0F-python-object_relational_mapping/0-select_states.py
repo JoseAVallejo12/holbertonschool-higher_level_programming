4356610a34d19d83875dfa065f6193fca95e3760
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-08 08:41:27 -0500

    add argv for handle funtion task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,14 @@
 # Show lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-            host="localhost", 
+            host="localhost",
             port=3306,
-            user="root",
-            passwd="root",
-            db="hbtn_0e_0_usa",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
             charset="utf8"
         )
 
@@ -17,6 +18,5 @@ if __name__ == "__main__":
     query_table = cur.fetchall()
     for row in query_table:
         print(row)
-
     cur.close()
     conn.close()
