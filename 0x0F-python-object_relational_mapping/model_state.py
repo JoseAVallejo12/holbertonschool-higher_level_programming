e012347c741b316ab7132581d4931c3fb18cd553
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-08 17:45:57 -0500

    create task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class base for SQLAlchemy."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """New Class State."""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
