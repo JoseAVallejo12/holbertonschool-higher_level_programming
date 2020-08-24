9166683f2a863e54ed551239b0c4dcea76906aae
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 16:54:30 -0500

    fix ..

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     dato = sys.argv[1] if len(sys.argv) == 2 else ""
     params = {'q': dato}
     response = requests.post('http://0.0.0.0:80/search_user', data=params)
-    if len(response.json()) != 0:
+    try:
         data = response.json()
-        if data is not dict:
-            print("Not a valid JSON")
-        print("[{}] {}".format(data.get('id'), data.get('name')))
-    else:
-        print("No result")
+        if len(data) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+    except Exception:
+        print("Not a valid JSON")
