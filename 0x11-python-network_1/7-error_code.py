879f9fe798f4c96b7a837bccdbcb386733d7691d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 15:15:34 -0500

    create task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that fetches to url and get X-Request-Id."""
+import requests
+import sys
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    if response.status_code > 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
