970572df1642c9b355908ed1a731071d490b910d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 14:18:32 -0500

    fix task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status."""
+from urllib import request, parse
+import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            the_page = response.read().decode('utf-8')
+            print(the_page)
+    except Exception as a:
+        print("Error code: {}".format(a.code))
