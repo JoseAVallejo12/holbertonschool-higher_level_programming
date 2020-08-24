38ef494a503c5a038265e8c59b28536c57b5dd7d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 14:35:37 -0500

    fix argv task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Script that fetches to url and get X-Request-Id."""
 import requests
+import sys
 
 if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
