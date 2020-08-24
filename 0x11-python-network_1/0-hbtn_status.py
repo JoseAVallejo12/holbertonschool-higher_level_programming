b31726c768a63e2e315d09248fd97559a89c3666
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 12:31:42 -0500

    indent print..

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,5 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-
-    print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}"""
-          .format(type(html), html, html.decode("utf-8"), end=""))
+    s = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}\n"
+    print(s.format(type(html), html, html.decode("utf-8"), end=""))
