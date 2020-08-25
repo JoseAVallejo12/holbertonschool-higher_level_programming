d2f58f401b477f5c14d2c4d37c3b7d59b160d0b2
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-24 20:06:52 -0500

    create task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Use github APi for list the 10 las commit in user and repo given."""
+import requests
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    response = requests.get(
+        'https://api.github.com/repos/{}/{}/commits'.format(
+            user, repo))
+    data = response.json()
+    for idx in range(10):
+        print("{}\t{}".format(
+            data[idx].get('sha'),
+            data[idx].get('commit').get('author').get('name')))
