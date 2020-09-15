75ae8cf196d4d707ec96e796dd55ea03e465f5df
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-15 13:29:02 -0500

    create task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+/* Write a function that returns the addition of 2 integers.
+The function must be visible from outside */
+const add = function (a, b) {
+  return a + b;
+};
+module.exports = { add };
