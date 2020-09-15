fa34187810c8efb534694797ccfe8633cd684ed1
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-15 12:25:00 -0500

    fix output task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,5 +3,9 @@
 if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
-  console.log('Argument found');
+  if (process.argv.length > 3) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
 }
