c1f4e84db9a8d7773029078f00a275f70319ba13
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-15 13:11:29 -0500

    create task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+/* Write a script that searches the second biggest integer in the list of arguments. */
+const arrNum = [];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 1; i < process.argv.length; i++) {
+    arrNum.push(parseInt(process.argv[i]));
+  }
+  if ((!arrNum) || (arrNum[0] === 0)) {
+    console.log(0);
+  } else {
+    console.log(
+      arrNum.sort((a, b) => {
+        return a - b;
+      })[arrNum.length - 2]
+    );
+  }
+}
