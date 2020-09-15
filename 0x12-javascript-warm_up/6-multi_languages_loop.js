f04a83212b0fa075e41c634c3464c8c55b614d78
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-15 12:28:02 -0500

    fix problem task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 /* Write a script that prints 3 lines:(like 1-multi_languages.js)
 but by using an array of string and a loop */
 const arrayTxt = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-arrayTxt.forEach((txt) => {
-  console.log(txt);
-});
+for (let i = 0; i < arrayTxt.length; i++) {
+  console.log(arrayTxt[i]);
+}
