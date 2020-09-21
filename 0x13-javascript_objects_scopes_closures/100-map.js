7c2813c3ff7b3eed6c52798e0bad4a25f81c43be
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-21 12:57:55 -0500

    fix style

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-/*Script that imports an array and computes a new array. */
-const list = require('./100-data').list;
-const newList = list.map((index, value) => index * value);
+/* Script that imports an array and computes a new array. */
+const { list } = require('./100-data');
 console.log(list);
-console.log(newList);
+console.log(list.map((index, value) => index * value));
