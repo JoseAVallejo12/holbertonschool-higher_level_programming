34d64aa331955c63579cffa87e1332ff09a9555b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 12:16:18 -0500

    fix styles

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 /* Write a script that reads and prints the content of a file. */
 var fs = require('fs');
 
-fs.readFile('./' + process.argv[2], 'utf8', function(err, data) {
+fs.readFile('./' + process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
