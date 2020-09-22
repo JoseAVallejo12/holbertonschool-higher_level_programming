4d61812fbe7585eb5d91948078c69c639cccd516
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 12:40:31 -0500

    create task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Write a script that writes a string to a file. */
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
