f88d944622d19100feb5904f3712f06083527bd5
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 12:17:27 -0500

    cahcge var for const

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* Write a script that reads and prints the content of a file. */
-var fs = require('fs');
+const fs = require('fs');
 
 fs.readFile('./' + process.argv[2], 'utf8', function (err, data) {
   if (err) {
