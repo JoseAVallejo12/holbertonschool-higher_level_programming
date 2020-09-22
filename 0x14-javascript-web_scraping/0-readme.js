c85e28bebb42f4f7f6af21392224b50e590927ac
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 12:29:13 -0500

    compact code in output console add or conditions

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 /* Write a script that reads and prints the content of a file. */
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  } else {
-    console.log(data);
-  }
+  console.log(data || err);
 });
