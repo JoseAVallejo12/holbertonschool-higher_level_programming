1b482c9226d612ee6e98ec059b1d59c58831fe4a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 12:27:29 -0500

    try again

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,8 @@ const fs = require('fs');
 
 fs.readFile('./' + process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    error = {
-        'Error': err.message + ' at Error (native)',
-        'errno': err.errno,
-        'code': err.code,
-        'syscall': err.syscall,
-        'path': err.path
-    }
-    return console.log(error);
+    return console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
