0c0c3e5444e3a01f481b0ec53b46954744ad65b3
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 20:36:07 -0500

    create task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,13 +2,19 @@
 /* script that prints all characters of a Star Wars movie: */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get({ url: url, json: true },
-  function (err, res, body) {
+
+function name (list, idx) {
+  request.get({ url: list[idx], json: true }, (err, res) => {
     if (err) console.log(err);
-    body.characters.forEach(element => {
-      request.get({ url: element, json: true }, (err, res) => {
-        if (err) console.log(err);
-        console.log(res.body.name);
-      });
-    });
+    console.log(res.body.name);
+    if (idx + 1 < list.length) {
+      name(list.shift(), idx++);
+    }
   });
+}
+
+request.get({ url: url, json: true }, function (err, res, body) {
+  if (err) console.log(err);
+  console.log(body.characters);
+  name(body.characters, 0);
+});
