2b751404b52d4ffd1f3dae30defad24ba9a32a32
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 20:58:28 -0500

    finish proyect

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 /* script that prints all characters of a Star Wars movie: */
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get({ url: url, json: true }, function (err, res, body) {
   if (err) console.log(err);
   print(body.characters, 0);
 });
 
-function print(characterList, index) {
-    request({url:characterList[index], json:true}, (err, res) => {
-        if (err) console.log(err);
-        console.log(res.body.name);
-        if (index + 1 < characterList.length)
-            print(characterList, index + 1)
-    });
+function print (characterList, index) {
+  request({ url: characterList[index], json: true }, (err, res) => {
+    if (err) console.log(err);
+    console.log(res.body.name);
+    if (index + 1 < characterList.length) { print(characterList, index + 1); }
+  });
 }
