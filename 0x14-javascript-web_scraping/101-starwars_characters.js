2e9114f774d3180eb260a087f8249b13c09f7745
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-22 20:57:29 -0500

    implement recursion to resolve problem and run sync code

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 /* script that prints all characters of a Star Wars movie: */
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-function name (list, idx) {
-  request.get({ url: list[idx], json: true }, (err, res) => {
-    if (err) console.log(err);
-    console.log(res.body.name);
-    if (idx + 1 < list.length) {
-      name(list.shift(), idx++);
-    }
-  });
-}
-
+const request = require("request");
+const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
 request.get({ url: url, json: true }, function (err, res, body) {
   if (err) console.log(err);
-  console.log(body.characters);
-  name(body.characters, 0);
+  print(body.characters, 0);
 });
+
+function print(characterList, index) {
+    request({url:characterList[index], json:true}, (err, res) => {
+        if (err) console.log(err);
+        console.log(res.body.name);
+        if (index + 1 < characterList.length)
+            print(characterList, index + 1)
+    });
+}
